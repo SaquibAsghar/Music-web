924e475671a5293a2c6a1054f417266b08b75e4c
Syed Saquib Asghar <saq9516@gmail.com>
2024-03-03 22:09:17 +0530

    work on navigation bar

--- src/app/chai/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function ChaiPage() {
-  return <h1 className="text-center text-3xl mt-9">Chai Page</h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="text-3xl text-center mt-9">Chai Aur Code</h1>;
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className=" text-3xl text-center mt-9">Home Page</h1>
+    </main>
+  );
 }
