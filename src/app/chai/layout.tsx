f2e55eb37dbdbc5c5eda4be96bda2814c4c98cd3
Syed Saquib Asghar <saq9516@gmail.com>
2024-03-03 20:50:32 +0530

    project init

---
+++ src/app/chai/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <h2>Inner layout of chai</h2>
+      {children}
+    </>
+  );
+}
---
+++ src/app/chai/page.tsx
@@ -0,0 +1,3 @@
+export default function ChaiPage() {
+  return <h1 className="text-center text-3xl mt-9">Chai Page</h1>;
+}
